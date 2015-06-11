ddec89d4984bfc94b975497d3474f7be868be3e4
brett <thebrettd@gmail.com>
2015-06-11 15:27:06 -0700

    Handle inputs from form

--- src/main/java/obfusc8r/Encoder.java
+++ src/main/java/obfusc8r/Encoder.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.Date;
 
 @Controller
 public class Encoder{
@@ -15,20 +16,23 @@ public class Encoder{
     }
 
     @RequestMapping(value="/encode", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
-                                                 @RequestParam("file") MultipartFile file){
+    public @ResponseBody String handleFileUpload(@RequestParam("message") String message,
+                                                 @RequestParam("file") MultipartFile file,
+                                                 @RequestParam("password") String password)
+    {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name)));
+                String fileName = new Date() + "-encoded.jpg";
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(fileName)));
                 stream.write(bytes);
                 stream.close();
-                return "You successfully uploaded " + name + "!";
+                return "You successfully encoded "  + message + " into file " + fileName + " with password " + password;
             } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
+                return "You failed to encode message " + message + " => " + e.getMessage();
             }
         } else {
-            return "You failed to upload " + name + " because the file was empty.";
+            return "You failed to encode " + message + " because the file was empty.";
         }
     }
 
